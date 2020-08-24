56b1a9aae94eed6644fb9ba4e500822a95a33857
Robertstein <robertovenezia95@gmail.com>
2020-08-24 19:01:16 +0200

    Close combat setup

---
+++ Ninja/Source/NinjaTest/NinjaTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NinjaTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANinjaTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANinjaTestGameMode();
+};
+
+
+
